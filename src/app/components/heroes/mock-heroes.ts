3bdf9ad748012aaaab2dc14e87eeb9cb54487213
brandonj17 <jonesbrandon166@yahoo.com>
2021-06-07 16:03:31 +0000

    Created Hero List &Basic  CSS

---
+++ src/app/components/heroes/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Iron Man' },
+  { id: 12, name: 'Hulk' },
+  { id: 13, name: 'Spider Man' },
+  { id: 14, name: 'Hawkeye' },
+  { id: 15, name: 'Super Man' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Black Panther' },
+  { id: 18, name: 'Deadpool' },
+  { id: 19, name: 'Thor' },
+  { id: 20, name: 'Dr. Strange' }
+];
