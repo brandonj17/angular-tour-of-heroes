469f7cd6067093ce4a86c57b3982e2ede0a2425f
brandonj17 <jonesbrandon166@yahoo.com>
2021-06-07 20:32:39 +0000

    Nav bar Finished?

--- src/app/components/hero-detail/hero-detail.component.ts
+++ src/app/components/hero-detail/hero-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroService } from 'src/app/hero.service';
 import { Hero } from '../heroes/hero';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-hero-detail',
@@ -7,10 +10,24 @@ import { Hero } from '../heroes/hero';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-  @Input() hero?: Hero;
-  constructor() { }
+  hero: Hero | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location) { }
 
   ngOnInit(): void {
+    this.getHero();
   }
 
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+  
+  goBack(): void {
+    this.location.back();
+  }
 }
